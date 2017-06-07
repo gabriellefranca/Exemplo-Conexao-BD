b2da27ac5934c5c5ec9ac14d8db126bb7c17f580
Gabrielle França <igabriellefranca@gmail.com>
2017-06-07 11:15:50 -0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,6 @@
-package com.example.aluno.login;
-
-import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.ConnectivityManager;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v7.app.NotificationCompat;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.gabi2.login.R;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    public static final String LOGIN_URL = "http://switchautos.com.br/login_mobile.php";
+    public static final String LOGIN_URL = "http://seu url/login.php";
 
     public static final String KEY_USERNAME = "username";
     public static final String KEY_PASSWORD = "password";
@@ -61,52 +29,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     }
 
-
-    public void gerarNotificacao(){
-
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        PendingIntent p = PendingIntent.getActivity(this, 0, new Intent(this, Main.class), 0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-        builder.setTicker("Ticker Texto");
-        builder.setContentTitle("Switch");
-        //builder.setContentText("Descrição");
-        builder.setSmallIcon(R.drawable.car);
-        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.car));
-        builder.setContentIntent(p);
-
-        NotificationCompat.InboxStyle style = new NotificationCompat.InboxStyle();
-        String [] descs = new String[]{"Seja bem Vindo", "Aproveite nosso aplicativo!"};
-        for(int i = 0; i < descs.length; i++){
-            style.addLine(descs[i]);
-        }
-        builder.setStyle(style);
-
-        Notification n = builder.build();
-        n.vibrate = new long[]{150, 300, 150, 600};
-        n.flags = Notification.FLAG_AUTO_CANCEL;
-        nm.notify(R.drawable.car, n);
-
-        try{
-            Uri som = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            Ringtone toque = RingtoneManager.getRingtone(this, som);
-            toque.play();
-        }
-        catch(Exception e){}
-    }
-
-    public void verificaConexao() {
-        ConnectivityManager conectivtyManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (conectivtyManager.getActiveNetworkInfo() != null
-                && conectivtyManager.getActiveNetworkInfo().isAvailable()
-                && conectivtyManager.getActiveNetworkInfo().isConnected()) {
-            conectado = 1;
-        } else {
-            conectado = 0;
-        }
-    }
-
-
     private void userLogin() {
         username = editTextUsername.getText().toString().trim();
         password = editTextPassword.getText().toString().trim();
@@ -122,7 +44,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                             //Intent troca = new Intent(MainActivity.this,UserProfile.class);
                             //startActivity(troca);
 
-                            Intent it = new Intent(MainActivity.this, Main.class);
+                            Intent it = new Intent(MainActivity.this, UserProfile.class);
                             it.putExtra(KEY_USERNAME, username);
                             startActivity(it);
 
@@ -158,24 +80,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private Toast toast;
     private long lastBackPressTime = 0;
 
-
-    public void onBackPressed() {
-        if (this.lastBackPressTime < System.currentTimeMillis() - 4000) {
-            toast = Toast.makeText(this, "Pressione o Botão Voltar novamente para fechar o Aplicativo.", Toast.LENGTH_LONG);
-            toast.show();
-            this.lastBackPressTime = System.currentTimeMillis();
-        } else {
-            if (toast != null) {
-                toast.cancel();
-            }
-            super.onBackPressed();
-        }
     }
 
     @Override
     public void onClick(View v) {
         userLogin();
-        gerarNotificacao();
     }
 
 
